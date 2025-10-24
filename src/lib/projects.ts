25fe52f5f8e9ac0c57e41182340ea9bccdd89b0a
Hammad hoath <hammadhoath123@gmail.com>
2025-10-24 16:14:11 +0500

    added projects refrence

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -3,7 +3,7 @@ export const projects = [
     title: "Portfolio Website",
     description:
       "A modern portfolio built with Next.js, Tailwind, and Framer Motion.",
-    image: "/images/project1.png",
+    image: "/images/portfolio-pic.jpg",
     demo: "portfolio-website-pied-eight-68.vercel.app",
     github: "https://github.com/hoath101/Portfolio-Website.git",
   },
@@ -11,8 +11,8 @@ export const projects = [
     title: "E-Commerce Dashboard",
     description:
       "Full-stack app using Next.js, Prisma, and PostgreSQL for real-time analytics.",
-    image: "/images/project2.png",
-    demo: "https://demo.com",
-    github: "https://github.com/johndoe/dashboard",
+    image: "/images/project-2.jpg",
+    demo: "hackathon-smoky-chi.vercel.app",
+    github: "https://github.com/hoath101/hackathon.git",
   },
 ];
