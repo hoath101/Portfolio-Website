1e9b5a49ee4d0be981608c1bcbc7f1cae53d9b53
Hammad hoath <hammadhoath123@gmail.com>
2025-10-24 15:55:31 +0500

    debloyed on vercel

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -4,8 +4,8 @@ export const projects = [
     description:
       "A modern portfolio built with Next.js, Tailwind, and Framer Motion.",
     image: "/images/project1.png",
-    demo: "https://myportfolio.com",
-    github: "https://github.com/johndoe/portfolio",
+    demo: "portfolio-website-pied-eight-68.vercel.app",
+    github: "https://github.com/hoath101/Portfolio-Website.git",
   },
   {
     title: "E-Commerce Dashboard",
